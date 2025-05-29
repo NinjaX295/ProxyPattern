2ab602e951094d4cd07579bb63c0f9fc4db0eb1c
KakshiDEV56 <kanitesh427@gmail.com>
2025-05-29 21:55:08 +0530

    added functions

--- TransparentPrxoy/v1Logic.sol
+++ TransparentPrxoy/v1Logic.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 contract LogicV1 {
     uint256 public value;
+    address implementationAddress;
+
 
     function setValue(uint256 _val) public {
         value = _val;
@@ -11,4 +13,13 @@ contract LogicV1 {
     function getValue() public view returns (uint256) {
         return value;
     }
+    function updateAddress(address updatedAddress)external onlyowner{
+        
+   implementationAddress=updatedAddress;
+  
+    }
+    function ViewImplementationAddress()public pure view{
+        return implementationAddress;
+    }
+    
 }
